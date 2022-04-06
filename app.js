5d4f2d486c4a95bc135a3d9f4494e3cc6b14fb6a
Sugarbayar <dorjsugaraaz@gmail.com>
2022-04-06 10:05:17 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -8,15 +8,20 @@ var score = [0, 0];
 var roundscore = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувчсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random()*6)+1;
 
 // Програм эхлэхэд бэлтгэе
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = '0';
+document.getElementById("score-1").textContent = '0';
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = '0';
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none"; 
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    var diceNumber = Math.floor(Math.random()*6)+1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+});
 
-document.querySelector(".dice").style.display = "none";    
 
-console.log(dice);
+
